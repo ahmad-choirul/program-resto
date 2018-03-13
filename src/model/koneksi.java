a7bdb7d1a9479c3d12d0570f9ccb9935d64e3660
ahmad-choirul <31815400+ahmad-choirul@users.noreply.github.com>
2018-03-14 00:55:41 +0700

    Initial commit

---
+++ src/model/koneksi.java
@@ -0,0 +1,91 @@
+package model;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+
+public class koneksi {
+
+    Connection con;
+    Statement stm;
+    java.sql.Connection connection;
+
+    public koneksi() throws SQLException {
+        String url = "jdbc:mysql://localhost/dbresto"; //url DB
+        String username = "root"; //username DB
+        String pass = ""; //password DB, Jika tidak di Password silahkan di Kosongkan.
+        //membuat koneksi ke DB
+        this.con = (Connection) DriverManager.getConnection(url, username, pass);
+        this.stm = (Statement) this.con.createStatement();
+    }
+
+    public java.sql.Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/dbresto", "root", "");
+        } catch (Exception e) {
+        }
+        return connection;
+    }
+
+    //method untuk ekesekusi query Insert, Update, dan Delete
+    public void executeQuery(String query) throws SQLException {
+        this.stm.execute(query);
+    }
+
+    //method untuk ekesekusi query Select
+    public ResultSet getResult(String query) throws SQLException {
+        ResultSet rs = stm.executeQuery(query);
+        return rs;
+    }
+
+    public boolean execute(String query) {
+        boolean sukseseksekusi;
+        try {
+            executeQuery(query);
+            sukseseksekusi = true;
+            System.out.println("query berhasil");
+        } catch (SQLException ex) {
+            sukseseksekusi = false;
+            System.out.println("query salah");
+        }
+
+        return sukseseksekusi;
+    }
+
+    public String[] getdataid(String query, String data[]) throws SQLException {
+        ResultSet rs = getResult(query);
+        if (rs.next()) {
+            for (int i = 0; i < data.length; i++) {
+                data[i] = rs.getString(i + 1);
+            }
+        }
+
+        return data;
+    }
+
+    public DefaultTableModel getDatatotal(String query, String kolom[]) throws SQLException {
+        DefaultTableModel table = new DefaultTableModel(null, kolom);
+        ResultSet rs = getResult(query);
+        while (rs.next()) {
+            String row[] = new String[kolom.length];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = rs.getString(i + 1);
+            }
+            table.addRow(row);
+        }
+        return table;
+    }
+
+    public String getdataidNoaray(String query) throws SQLException {
+        ResultSet rs = getResult(query);
+        String data = "";
+        if (rs.next()) {
+            data = rs.getString(1);
+        }
+        return data;
+    }
+}
---
+++ src/model/mpesanan.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author acer
+ */
+public class mpesanan extends koneksi {
+
+    public mpesanan() throws SQLException {
+        super();
+    }
+
+    public boolean tambahpesananminum(String data[]) {
+        //data[0]=id_pesanan
+        //data[1]=id_minumman
+        //data[2]=harga_bayar
+        String query = "INSERT INTO `pesanan` (`id_daftar_pesanan`, `id_pesanan`, `id_menu`, `id_pesan_toping`, `id_minuman`, `level`, `harga_bayar`) "
+                + "VALUES (NULL, '" + data[0] + "', NULL, NULL, '" + data[1] + "', NULL, '" + data[2] + "');;";
+        return execute(query);
+    }
+    public boolean tambahpesananmakan(String data[]) {
+        //data[0]=id_pesanan
+        //data[1]=id_menu
+        //data[2]=id_toping
+        //data[3]=level
+        //data[4]=harga_bayar
+        String query = "INSERT INTO `pesanan` (`id_daftar_pesanan`, `id_pesanan`, `id_menu`, `id_pesan_toping`, `id_minuman`, `level`, `harga_bayar`) "
+                + "VALUES (NULL, '" + data[0] + "', '" + data[1] + "','" + data[2] + "', NULL,'" + data[3] + "', '" + data[4] + "');";
+        System.out.println("query insert makan "+query);
+        return execute(query);
+    }
+    public boolean tambahtoping(String data[]) {
+        //data[0]=id_pesan_toping
+        //data[1]=id_topingn
+        String query = "INSERT INTO `pesan_toping` (`id_table_toping`, `id_pesan_toping`, `id_toping`) "
+                + "VALUES (NULL, '" + data[0] + "', '" + data[1] + "');";
+        return execute(query);
+    }
+
+    public boolean delete(String data) {
+        String query = "DELETE FROM `pesanan` WHERE `pesanan`.`id_daftar_pesanan` = " + data;
+        return execute(query);
+    }
+    public boolean deletetoping(String data) {
+        String query = "DELETE FROM `pesan_toping` WHERE `pesan_toping`.`id_table_toping` = " + data;
+        return execute(query);
+    }
+    public DefaultTableModel cekpesananmakanan(String data) throws SQLException {
+        String kolom[] = {"no", "makanan", "level", "id toping"};
+        String query = "SELECT id_daftar_pesanan,m.nama_menu,level,id_pesan_toping FROM `pesanan` p join menu m on (p.id_menu=m.id_menu) where p.id_menu is not null and id_pesanan = " + data;
+        return getDatatotal(query, kolom);
+    }
+    public DefaultTableModel cekpesananminuman(String data) throws SQLException {
+        String kolom[] = {"no", "minuman"};
+        String query = "SELECT id_daftar_pesanan,m.nama_minuman FROM `pesanan` p join minuman m on "
+                + "(m.id_minuman=p.id_minuman) where id_pesanan = " + data;
+        return getDatatotal(query, kolom);
+    }
+     public DefaultTableModel gettoping( String data) throws SQLException {
+        String kolom[] = {"no", "minuman"};
+        String query = "SELECT `id_table_toping`,t.nama_toping FROM `pesan_toping` p join toping t on "
+                + "(p.id_toping=t.id_toping) WHERE `id_pesan_toping` = " + data;
+        return getDatatotal(query, kolom);
+    }
+
+    public String getidpesanan() {
+        String id = null;
+        try {
+            String query = "SELECT MAX(`id_pesanan`)+1 as id_pesanan FROM `pesanan`";
+            id = getdataidNoaray(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(mpesanan.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
+    }
+    public String getidtoping() {
+        String id = null;
+        try {
+            String query = "SELECT MAX(`id_pesan_toping`)+1 as id_pesanan FROM `pesan_toping`";
+            id = getdataidNoaray(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(mpesanan.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
+    }
+}
